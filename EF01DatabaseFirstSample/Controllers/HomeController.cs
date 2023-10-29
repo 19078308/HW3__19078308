621111229192d74fb487d7ee3bfc504dc8cdffbe
Tshepo Mononyane <u19078308@up.ac.za>
2023-10-29 16:04:46 +0200

    #home#

---
+++ EF01DatabaseFirstSample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using EF01DatabaseFirstSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using System.Web.UI;
+
+namespace EF01DatabaseFirstSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly LibraryEntities libraryEntities;
+        // GET: Home
+        private LibraryEntities _context;
+
+        public HomeController()
+        {
+            _context = new LibraryEntities(); // Initialize your DbContext
+        }
+        public ActionResult Home()
+        {
+            //int pageSize = 5;
+            //int pageNumber = (Page ?? 1);
+            // Retrieve data for Students and Books from your database
+            List<students> students = _context.students.ToList();
+            List<books> books = _context.books.ToList();
+
+            HomeViewModel viewModel = new HomeViewModel
+            {
+                Students = students,
+                Books = books
+            };
+            //IPagedList<students> pagedStudents = students.ToPagedList(pageNumber, pageSize);
+
+
+            return View("Home", viewModel);
+        }
+    }
+}
---
+++ EF01DatabaseFirstSample/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF01DatabaseFirstSample.Models
+{
+    public class HomeViewModel
+    {
+        public List<students> Students { get; set; }
+        public List<books> Books { get; set; }
+    }
+}
